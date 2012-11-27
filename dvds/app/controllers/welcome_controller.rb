c5d8399b7c4b95a3637e2dc5fb1726de999edc93
Adrian Mann <adrianmatthiasmann@hotmail.com>
2012-11-27 12:11:33 +0000

    tutorial1_and_tutorial2

---
+++ dvds/app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  def home
+  end
+  def thank_you
+  end
+  def register
+  end
+end
---
+++ dvds/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dvds::Application.config.secret_token = '7bf588f5555cbdc2dba314bfb93dbe5fa9c331f851cf2f53cd7637753c5b2dcaf73e8d76a99058bd53b806689fabbdee9aac59421aecfb21b2553a4f5badaa60'
