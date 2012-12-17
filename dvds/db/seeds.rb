5e0770408cbeecf4fcf3c76f56308b83f8c8ebf7
philipbreslin <philipbreslin@hotmail.com>
2012-12-17 15:16:05 +0000

    sunday16th and monday 17th work

--- dvds/db/seeds.rb
+++ dvds/db/seeds.rb
@@ -23,3 +23,191 @@ Product.create(:name => '1968 All-Ireland Football Final', :description =>
     'Down v Kerry', :price => '9.99')
 Product.create(:name => '1969 All-Ireland Football Final', :description =>
     'Kerry v Offaly', :price => '9.99')
+Product.create(:name => '1970 All-Ireland Football Final', :description =>
+    'Kerry v Meath', :price => '9.99')
+Product.create(:name => '1971 All-Ireland Football Final', :description =>
+    'Offaly v Galway', :price => '9.99')
+Product.create(:name => '1972 All-Ireland Football Final', :description =>
+    'Offaly v Kerry - Draw and Replay Double DVD', :price => '14.99')
+Product.create(:name => '1973 All-Ireland Football Final', :description =>
+    'Cork v Galway', :price => '9.99')
+Product.create(:name => '1974 All-Ireland Football Final', :description =>
+    'Dublin v Galway', :price => '9.99')
+Product.create(:name => '1975 All-Ireland Football Final', :description =>
+    'Kerry v Dublin', :price => '9.99')
+Product.create(:name => '1976 All-Ireland Football Final', :description =>
+    'Dublin v Kerry', :price => '9.99')
+Product.create(:name => '1977 All-Ireland Football Final', :description =>
+    'Dublin v Kerry', :price => '9.99')
+Product.create(:name => '1978 All-Ireland Football Final', :description =>
+    'Kerry v Dublin', :price => '9.99')
+Product.create(:name => '1979 All-Ireland Football Final', :description =>
+    'Kerry v Dublin', :price => '9.99')
+Product.create(:name => '1980 All-Ireland Football Final', :description =>
+    'Kerry v Roscommon', :price => '9.99')
+Product.create(:name => '1981 All-Ireland Football Final', :description =>
+    'Kerry v Offaly', :price => '9.99')
+Product.create(:name => '1982 All-Ireland Football Final', :description =>
+    'Offaly v Kerry', :price => '9.99')
+Product.create(:name => '1983 All-Ireland Football Final', :description =>
+    'Dublin v Galway', :price => '9.99')
+Product.create(:name => '1984 All-Ireland Football Final', :description =>
+    'Kerry v Dublin', :price => '9.99')
+Product.create(:name => '1985 All-Ireland Football Final', :description =>
+    'Kerry v Dublin', :price => '9.99')
+Product.create(:name => '1986 All-Ireland Football Final', :description =>
+    'Kerry v Tyrone', :price => '9.99')
+Product.create(:name => '1987 All-Ireland Football Final', :description =>
+    'Meath v Cork', :price => '9.99')
+Product.create(:name => '1988 All-Ireland Football Final', :description =>
+    'Meath v Cork - Draw and Replay Double DVD', :price => '14.99')
+Product.create(:name => '1989 All-Ireland Football Final', :description =>
+    'Cork v Mayo', :price => '9.99')
+Product.create(:name => '1990 All-Ireland Football Final', :description =>
+    'Cork v Meath', :price => '9.99')
+Product.create(:name => '1991 All-Ireland Football Final', :description =>
+    'Down v Meath', :price => '9.99')
+Product.create(:name => '1992 All-Ireland Football Final', :description =>
+    'Donegal v Dublin', :price => '9.99')
+Product.create(:name => '1993 All-Ireland Football Final', :description =>
+    'Derry v Cork', :price => '9.99')
+Product.create(:name => '1994 All-Ireland Football Final', :description =>
+    'Down v Dublin', :price => '9.99')
+Product.create(:name => '1995 All-Ireland Football Final', :description =>
+    'Dublin v Tyrone', :price => '9.99')
+Product.create(:name => '1996 All-Ireland Football Final', :description =>
+    'Meath v Mayo - Draw and Replay Double DVD', :price => '14.99')
+Product.create(:name => '1997 All-Ireland Football Final', :description =>
+    'Kerry v Mayo', :price => '9.99')
+Product.create(:name => '1998 All-Ireland Football Final', :description =>
+    'Galway v Kildare', :price => '9.99')
+Product.create(:name => '1999 All-Ireland Football Final', :description =>
+    'Meath v Cork', :price => '9.99')
+Product.create(:name => '2000 All-Ireland Football Final', :description =>
+    'Kerry v Galway - Draw and Replay Double DVD', :price => '14.99')
+Product.create(:name => '2001 All-Ireland Football Final', :description =>
+    'Galway v Meath', :price => '9.99')
+Product.create(:name => '2002 All-Ireland Football Final', :description =>
+    'Armagh v Kerry', :price => '9.99')
+Product.create(:name => '2003 All-Ireland Football Final', :description =>
+    'Tyrone v Armagh', :price => '9.99')
+Product.create(:name => '2004 All-Ireland Football Final', :description =>
+    'Kerry v Mayo', :price => '9.99')
+Product.create(:name => '2005 All-Ireland Football Final', :description =>
+    'Tyrone v Kerry', :price => '9.99')
+Product.create(:name => '2006 All-Ireland Football Final', :description =>
+    'Kerry v Mayo', :price => '9.99')
+Product.create(:name => '2007 All-Ireland Football Final', :description =>
+    'Kerry v Cork', :price => '9.99')
+Product.create(:name => '2008 All-Ireland Football Final', :description =>
+    'Tyrone v Kerry', :price => '9.99')
+Product.create(:name => '2009 All-Ireland Football Final', :description =>
+    'Kerry v Cork', :price => '9.99')
+Product.create(:name => '2010 All-Ireland Football Final', :description =>
+    'Cork v Down', :price => '9.99')
+Product.create(:name => '2011 All-Ireland Football Final', :description =>
+    'Dublin v Kerry', :price => '9.99')
+Product.create(:name => '2012 All-Ireland Football Final', :description =>
+    'Donegal v Mayo', :price => '9.99')
+Product.create(:name => '1962 All-Ireland Hurling Final', :description =>
+    'Tipperary v Wexford', :price => '9.99')
+Product.create(:name => '1963 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Waterford', :price => '9.99')
+Product.create(:name => '1964 All-Ireland Football Final', :description =>
+    'Tipperary v Kilkenny', :price => '9.99')
+Product.create(:name => '1965 All-Ireland Hurling Final', :description =>
+    'Tipperary v Wexford', :price => '9.99')
+Product.create(:name => '1966 All-Ireland Hurling Final', :description =>
+    'Cork v Kilkenny', :price => '9.99')
+Product.create(:name => '1967 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Tipperary', :price => '9.99')
+Product.create(:name => '1968 All-Ireland Hurling Final', :description =>
+    'Wexford v Tipperary', :price => '9.99')
+Product.create(:name => '1969 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Cork', :price => '9.99')
+Product.create(:name => '1970 All-Ireland Hurling Final', :description =>
+    'Cork v Wexford', :price => '9.99')
+Product.create(:name => '1971 All-Ireland Hurling Final', :description =>
+    'Tipperary v Kilkenny', :price => '9.99')
+Product.create(:name => '1972 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Cork', :price => '9.99')
+Product.create(:name => '1973 All-Ireland Hurling Final', :description =>
+    'Limerick v Kilkenny', :price => '9.99')
+Product.create(:name => '1974 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Limerick', :price => '9.99')
+Product.create(:name => '1975 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Galway', :price => '9.99')
+Product.create(:name => '1976 All-Ireland Hurling Final', :description =>
+    'Cork v Wexford', :price => '9.99')
+Product.create(:name => '1977 All-Ireland Hurling Final', :description =>
+    'Cork v Wexford', :price => '9.99')
+Product.create(:name => '1978 All-Ireland Hurling Final', :description =>
+    'Cork v Kilkenny', :price => '9.99')
+Product.create(:name => '1979 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Galway', :price => '9.99')
+Product.create(:name => '1980 All-Ireland Hurling Final', :description =>
+    'Galway v Limerick', :price => '9.99')
+Product.create(:name => '1981 All-Ireland Hurling Final', :description =>
+    'Offaly v Galway', :price => '9.99')
+Product.create(:name => '1982 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Cork', :price => '9.99')
+Product.create(:name => '1983 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Cork', :price => '9.99')
+Product.create(:name => '1984 All-Ireland Hurling Final', :description =>
+    'Cork v Offaly', :price => '9.99')
+Product.create(:name => '1985 All-Ireland Hurling Final', :description =>
+    'Offaly v Galway', :price => '9.99')
+Product.create(:name => '1986 All-Ireland Hurling Final', :description =>
+    'Cork v Galway', :price => '9.99')
+Product.create(:name => '1987 All-Ireland Hurling Final', :description =>
+    'Galway v Kilkenny', :price => '9.99')
+Product.create(:name => '1988 All-Ireland Hurling Final', :description =>
+    'Galway v Tipperary', :price => '9.99')
+Product.create(:name => '1989 All-Ireland Hurling Final', :description =>
+    'Tipperary v Antrim', :price => '9.99')
+Product.create(:name => '1990 All-Ireland Hurling Final', :description =>
+    'Cork v Galway', :price => '9.99')
+Product.create(:name => '1991 All-Ireland Hurling Final', :description =>
+    'Offaly v Kilkenny', :price => '9.99')
+Product.create(:name => '1992 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Cork', :price => '9.99')
+Product.create(:name => '1993 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Galway', :price => '9.99')
+Product.create(:name => '1994 All-Ireland Hurling Final', :description =>
+    'Offaly v Limerick', :price => '9.99')
+Product.create(:name => '1995 All-Ireland Hurling Final', :description =>
+    'Clare v Offaly', :price => '9.99')
+Product.create(:name => '1996 All-Ireland Hurling Final', :description =>
+    'Wexford v Limerick', :price => '9.99')
+Product.create(:name => '1997 All-Ireland Hurling Final', :description =>
+    'Clare v Tipperary', :price => '9.99')
+Product.create(:name => '1998 All-Ireland Hurling Final', :description =>
+    'Offaly v Kilkenny', :price => '9.99')
+Product.create(:name => '1999 All-Ireland Hurling Final', :description =>
+    'Cork v Kilkenny', :price => '9.99')
+Product.create(:name => '2000 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Offaly', :price => '9.99')
+Product.create(:name => '2001 All-Ireland Hurling Final', :description =>
+    'Tipperary v Galway', :price => '9.99')
+Product.create(:name => '2002 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Clare', :price => '9.99')
+Product.create(:name => '2003 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Cork', :price => '9.99')
+Product.create(:name => '2004 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Cork', :price => '9.99')
+Product.create(:name => '2005 All-Ireland Hurling Final', :description =>
+    'Cork v Galway', :price => '9.99')
+Product.create(:name => '2006 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Cork', :price => '9.99')
+Product.create(:name => '2007 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Limerick', :price => '9.99')
+Product.create(:name => '2008 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Waterford', :price => '9.99')
+Product.create(:name => '2009 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Tipperary', :price => '9.99')
+Product.create(:name => '2010 All-Ireland Hurling Final', :description =>
+    'Tipperary v Kilkenny', :price => '9.99')
+Product.create(:name => '2011 All-Ireland Hurling Final', :description =>
+    'Kilkenny v Tipperary', :price => '9.99')
+Product.create(:name => '2012 All-Ireland Hurling Final', :description =>
+    'Galway v Kilkenny - Draw and Replay Double DVD', :price => '14.99')
